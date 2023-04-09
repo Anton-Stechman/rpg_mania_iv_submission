16b6bdbf969cdbb20708e6dbec526533fa2da8dc
Anton Stechman <stechman.ab@gmail.com>
2023-04-09 19:02:27 +1200

    U3DT Auto Commit Message: Intial Commit; Publishing Branch

---
+++ Assets/Code/Scripts/CSharp/Player/MouseLook.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using U3DT.Utils;
+using U3DT.Library;
+using System.Collections.Generic;
+
+public class MouseLook : MonoBehaviour {
+
+
+    //Code Generated from U3DT Player Template; https://github.com/Anton-Stechman/U3DTools/tree/main
+    private void Awake() {
+        // execute on awake (before game start)
+    }
+
+    private void Start() {
+        // execute on game start
+    }
+
+    private void FixedUpdate() {
+        // physics
+    }
+
+    private void Update() {
+        // Other
+    }
+
+    private void LateUpdate() {
+        // camera
+    }
+   
+}
